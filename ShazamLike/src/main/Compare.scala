49d09abca78f3a1359bdc747e146a04ec1675f4f
Anissa Bokhamy <anissa.bokhamy@telecomnancy.net>
2016-12-09 18:56:03 +0100

    Algo de comparaison

--- ShazamLike/src/main/Compare.scala
+++ ShazamLike/src/main/Compare.scala
@@ -1,5 +1,48 @@
 package main
+import scala.math._
+object Compare 
+{
 
-object Compare {
   
+ 
+def empreinte(T:Array[Array[Float]]):(Array[Array[Float]])=
+{
+  //on suppose que le tableau est de la forme T=[[A,f,t],[ ], ...]
+  //il faut que le tableau d'amplitudes soit trié en fonction du temps
+  //on réorganise les amplitudes en fonction des fréquences (croissantes) (c'est ce qui sort de la fft)
+  var TabBande=Array[Array[Float]]
+  var Tabmax=Array.emptyFloatArray
+  for (palier<-Array(10,20,40,80,160,511))
+  {
+    var compteur=0
+    var TabBande=Array.emptyObjectArray
+    while (math.log10(T(compteur)(1))<palier)
+    {
+      TabBande+:=T(i)
+      compteur+=1
+    }
+    Tabmax+:=maxF(TabBande) //créer la fonction maxF d'un tableau
+  }
+  var moy=moyenne(Tabmax) //creer la fonction moyenne d'un tableau
+  for (i<-0 to Tabmax.length-1)
+  {
+    if (Tabmax(i)(0)<moy) //on fait la moyenne des amplitudes
+    {
+      Tabmax-=Tabmax(i)
+    }
+  }
+  return Tabmax
+  }
+  
+  //on découpe l'étendue des fréquences en 6 bouts en échelle logarithme
+  //on réalise une fonction max d'un tableau sur les amplitudes de chaque bande de fréquence
+  //on calcule la moyenne des 6 valeurs obtenues
+  //on garde seulement les points qui sont au dessus de la moyenne
+  //on obtient notre empreinte
+  
+}
+
+
+
+
 }
