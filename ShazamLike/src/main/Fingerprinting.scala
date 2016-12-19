5b29b0e241783cb97d3cfe10414c470658c37c44
Pierre Kovarski <pierre.kovarski@telecomnancy.eu>
2016-12-19 16:33:57 +0100

    Tableau trié comme selon le site mais correction à apporter, création des target zones

--- ShazamLike/src/main/Fingerprinting.scala
+++ ShazamLike/src/main/Fingerprinting.scala
@@ -36,22 +36,43 @@ object Fingerprinting {
   //puis si deux points ont le meme temps, alors on trie selon la frequence
   //sinon on trie selon le temps
   
-def triTemps(T: Array[Array[Float]]) : Boolean = {
+  def triTemps(T: Array[Array[Float]]) : (Array[Array[Float]]) = {
   for (i<-T.length -1 to 1 by -1){
     var tableauTrie:Boolean = true
     for (j<- 0 to i-1) {
     if (T(2)(j+1) < T(2)(j){
-        echanger(T(2)(j+1),T(2)(j))// faire une fonction echanger
+        T=echanger(j+1,j,T)// echanger est tout en bas
         tableauTrie = false
         if (tableauTrie) {
-          return false
-        }
+          return T
+       }
+    }
+    }
   }
+  return T
   }
-}
-}// on obtient un tableau trie selon le temps 
+// on obtient un tableau trie selon le temps 
 // maintenant on parcourt ce tableau
 // et on trie si les points ont la meme frequence
+  def trifrequence(T:Array[Array[Float]]) : (Array[Array[Float]]) = {
+    for (i<-0 to T.length-1) {
+      if (T(2)(i) == T(2)(i+1) || T(1)(i) > T(1)(i+1)){ //si même temps mais la fq du pt d'avant est plus forte alors on echange
+        T=echanger(i,i+1,T)
+      }
+    }
+    return T // Ne marche pas si on a [A1,40,1] [A2,30,1] [A3,20,1] par exemple car sort [A2,30,1] [A3,20,1] [A1,40,1]
+  //A modifier
+  }
+  
+  def targetZone(T:Array[Array[Float]]) : (Array(Array(Array[Float]))) = {
+    //Selon le site une target zone est composee de 6 points donc on cree un tableau de tableaux de 6 points(avec point =[A,f,t]
+    var targetZones : Array(Array(Array[Float])) = Array(Array(Array()))
+    for (i<0 to T.length -7) {
+      targetZones(i) += T(i) + T(i+1) + T(i+2) + T(i+3) + T(i+4) + T(i+5)
+    }
+    return targetZones
+  }
+  //Le point d'ancrage est choisi comme étant le 3eme point avant le début de la target zone
     
   def selection(T : Array[Array[Float]], debut : Int, n : Int) : Array[Array[Float]] = {
     //fonction qui selectionne, a� partir d'un tableau T, n sous tableaux a partir de l'indice debut
@@ -97,5 +118,10 @@ def triTemps(T: Array[Array[Float]]) : Boolean = {
     return TabSup
   }
   
-  def echanger(
+  def echanger(indice1:Float,indice2:Float,T:Array[Array[Float]]) : (Array[Array[Float]]) = {
+    var save = T(indice1)
+    T(indice1)=T(indice2)
+    T(indice2) = save
+    return T
+  }
 }
