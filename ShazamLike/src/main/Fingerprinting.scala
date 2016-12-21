67870cc071ea29a6368203030fc081e216b79a78
Pierre Kovarski <pierre.kovarski@telecomnancy.eu>
2016-12-21 21:42:44 +0100

    Creation empreinte mais encore des bug à réparer

--- ShazamLike/src/main/Fingerprinting.scala
+++ ShazamLike/src/main/Fingerprinting.scala
@@ -8,37 +8,17 @@ object Fingerprinting {
   //puis si deux points ont le meme temps, alors on trie selon la frequence
   //sinon on trie selon le temps
    
-  def targetZone(T:Array[Array[Float]]) : (Array[Array[Array[Float]]]) = {
-    //Selon le site une target zone est composee de 6 points donc on cree un tableau de tableaux de 6 points(avec point =[A,f,t]
-    var targetZones : Array[Array[Array[Float]]] = Array(Array(Array()))
-    for (i<-0 to T.length -7) {
-      targetZones(i) += T(i) + T(i+1) + T(i+2) + T(i+3) + T(i+4) + T(i+5)
-    }
-    return targetZones
-  }
-  //Le point d'ancrage est choisi comme étant le 3eme point avant le début de la target zone
-  
-  
-  
+
   def triTemps(T: Array[Array[Float]]) : (Array[Array[Float]]) = {
-    var tab=T
-    for (i<-tab.length -1 to 1 by -1){
-    var tableauTrie:Boolean = true
-    for (j<- 0 to i-1) {
-    if (tab(2)(j+1) < tab(2)(j)){
-        tab=echanger(j+1,j,tab) // echanger est tout en bas
-        tableauTrie = false
-        if (tableauTrie) {
-          return tab
-       }
-    }
-    }
+    triBulle(T,2) // le tri est fait plus bas
   }
   return T
   }
 // on obtient un tableau trie selon le temps 
 // maintenant on parcourt ce tableau
 // et on trie si les points ont la meme frequence
+
+//on parcourt le tableau et on va mettre les points ayant meme t dans un autre tableau
   def trifrequence(T:Array[Array[Float]]) : (Array[Array[Float]]) = {
     var tab=T
     for (i<-0 to tab.length-1) {
@@ -49,12 +29,44 @@ object Fingerprinting {
     return T // Ne marche pas si on a [A1,40,1] [A2,30,1] [A3,20,1] par exemple car sort [A2,30,1] [A3,20,1] [A1,40,1]
   //A modifier
   }
- 
+    def targetZone(T:Array[Array[Float]]) : (Array[Array[Array[Float]]]) = {
+    //Selon le site une target zone est composee de 6 points donc on cree un tableau de tableaux de 6 points(avec point =[A,f,t]
+    var targetZones : Array[Array[Array[Float]]] = Array(Array(Array()))
+    for (i<-0 to T.length -7) {
+      targetZones(i) += T(i) + T(i+1) + T(i+2) + T(i+3) + T(i+4) + T(i+5)
+    }
+    return targetZones
+  }
+   //Le point d'ancrage est choisi comme étant le 3eme point avant le début de la target zone selon le site, 
+    //j'ai choisi de faire une empreinte avec chaque 2eme point de la taget zone
+    
+    def empreinte(targetZone:Array[Array[Array[Float]]],T:Array[Float])={
+      var emp:Array[Array[Float]]=Array(Array())
+      for (i<-3 to targetZone.length) {
+        emp += (T(i-3)(1),targetZone(i)(3)(1),targetZone(i)(3)(2)-T(i-3)(2)) // HELP JE SAIS PAS LECRIRE
+        }
+      return emp
+    }
+    
   def echanger(indice1:Int,indice2:Int,T:Array[Array[Float]]) : (Array[Array[Float]]) = {
     var save = T(indice1)
     T(indice1)=T(indice2)
     T(indice2) = save
     return T
+  }
+  
+  def triBulle(T:Array[Array[Float]],donnee:Int){
+    var tab=T
+    for (i<-tab.length -1 to 1 by -1){
+    var tableauTrie:Boolean = true
+    for (j<- 0 to i-1) {
+    if (tab(donnee)(j+1) < tab(donnee)(j)){
+        tab=echanger(j+1,j,tab) // echanger est en haut (a modifier ?)
+        tableauTrie = false
+        if (tableauTrie) {
+          return false //on arrete la boucle
+       }
+    }
   }
   
 
