ea2e5ab086abe3c27f844f54904ae9a7f531a764
Pierre Kovarski <pierre.kovarski@telecomnancy.eu>
2016-12-24 16:16:08 +0100

    fonction resume

--- ShazamLike/src/main/Fingerprinting.scala
+++ ShazamLike/src/main/Fingerprinting.scala
@@ -1,10 +1,29 @@
 package main
 
 import scala.math._
-
+// Point = [A,f,t]
+// 1)On obtient de la constellation une liste T=[[A,f,t],[A,f,t],[A,f,t]...]
+// 2)On trie T selon le temps
+// 3)On trie T selon les fréquences si le temps est égal
+// 4)On crée des targetzones de 6 points = [[1,2,3,4,5,6],[2,3,4,5,6,7],[3,4,5,6,7,8]]] avec 1=[A1,f1,t1] 2=[A2,f2,t2] etc
+// 5)On crée les empreintes
 object Fingerprinting {
+  //fonction qui résume
+  def resume(T:[Array[Array[Float]]):(Array[Array[Float])={
+    var tab = triTemps(T)
+    tab = triFrequence
+    targetzones=targetZone(tab)
+    fingerprint=empreinte(targetzones,tab)
+    return targetzones,fingerprint
+  }
+    
+    
+    
+    
+    
+    
   // creation de la target zone : on trie les points obtenus de cette maniere : 
-  // les points sont deja tries selon la frequence, on va d'abord les trier selon le temps par un tri a bulle
+  // les points sont deja tries selon la frequence, on va d'abord les trier selon le temps par un tri shell
   //puis si deux points ont le meme temps, alors on trie selon la frequence
   //sinon on trie selon le temps
    
@@ -18,7 +37,7 @@ object Fingerprinting {
 // et on trie si les points ont la meme frequence
 
 //on parcourt le tableau et on va mettre les points ayant meme t dans un autre tableau
-  def trifrequence(T:Array[Array[Float]]) : (Array[Array[Float]]) = {
+  def triFrequence(T:Array[Array[Float]]) : (Array[Array[Float]]) = {
     var tab=T
     for (i<-0 to tab.length-1) {
       if (tab(2)(i) == tab(2)(i+1) || tab(1)(i) > tab(1)(i+1)){ //si même temps mais la fq du pt d'avant est plus forte alors on echange
@@ -31,10 +50,11 @@ object Fingerprinting {
   
   
     def targetZone(T:Array[Array[Float]]) : (Array[Array[Array[Float]]]) = {
-    //Selon le site une target zone est composee de 6 points donc on cree un tableau de tableaux de 6 points(avec point =[A,f,t]
+      //Ex : [[1,2,3,4,5,6],[2,3,4,5,6,7],[3,4,5,6,7,8]]] avec 1=[A1,f1,t1] 2=[A2,f2,t2] etc
+      //Selon le site une target zone est composee de 6 points donc on cree un tableau de tableaux de 6 points(avec point =[A,f,t]
     var targetZones : Array[Array[Array[Float]]] = Array(Array(Array()))
     for (i<-0 to T.length -7) {
-      targetZones(i) = [T(i),T(i+1),T(i+2),T(i+3),T(i+4),T(i+5)]
+      targetZones(i) ++ = [T(i),T(i+1),T(i+2),T(i+3),T(i+4),T(i+5)]
     }
     return targetZones
   }
@@ -44,7 +64,7 @@ object Fingerprinting {
     def empreinte(targetZone:Array[Array[Array[Float]]],T:Array[Float])={
       var emp:Array[Array[Float]]=Array(Array())
       for (i<-3 to targetZone.length) {
-        emp += (T(i-3)(1),targetZone(i)(3)(1),targetZone(i)(3)(2)-T(i-3)(2)) // HELP JE SAIS PAS LECRIRE
+        emp ++ = (T(i-3)(1),targetZone(i)(3)(1),targetZone(i)(3)(2)-T(i-3)(2)) // HELP JE SAIS PAS LECRIRE
         }
       //on retourne [fréquence du point d'ancrage, fréquence du point de début de target zone ,delta temps entre le point d'ancrage et le point] 
       return emp
@@ -61,6 +81,7 @@ object Fingerprinting {
     var sublistcount = T.length//2
     while (sublistcount>0){
       for (startposition <- 0 to sublistcount){
+        print("After increment of size", sublistcount, "The list is",alist)
         gapInstertionSort(T,startposition,sublistcount)
         sublistcount = sublistcount //2
       }
@@ -68,7 +89,7 @@ object Fingerprinting {
     
   }
   
-  def gapInsertionSort((T:Array[Array[Float]],donnee:Int,start:Int,gap:Int){
+  def gapInsertionSort(T:Array[Array[Float]],donnee:Int,start:Int,gap:Int){
     var currentvalue = 0
     var position = 0
     for (i<-start+gap to T.length by gap){
