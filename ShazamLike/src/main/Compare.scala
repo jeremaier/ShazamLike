cdef0b541095f15ec94c97f2880890830e0be10f
Pierre Kovarski <pierre.kovarski@telecomnancy.eu>
2017-01-01 20:09:23 +0100

    comparaison

--- ShazamLike/src/main/Compare.scala
+++ ShazamLike/src/main/Compare.scala
@@ -1,131 +1,48 @@
 package main
 import scala.math._
+import scala.collection.mutable.ArrayBuffer
 object Compare 
 {
-//objectif: comparer un échantillon avec chaque morceau de la base de donnee
-//1: pour un echantillon, on calcule le taux le compatibilité avec chaque morceau de la base de donnee
-//2: on garde que ceux dont le taux est superieur à 90% et on garde les empreintes concernees
-//3: on vérifie la cohérence temporelle entre chaque empreinte de l'enchantillon et celle du morceau
-//4: on garde celle qui est la plus cohérente temporellement
-  // i.e. celle qui a le plus grand nombre de delta cohérent
-
-def matching(sample:Array[Array[Float]],database:Array[Array[Array[Float]]]):Float={
-  //fonction qui prend en entrée un échantillon sous forme d'empreinte
-  //et une base de donnée sous la forme [[[ID1],[empreinte1],[empreinte2]],[[ID2],[empreinte1],[empreinte2]]]
-  //et qui renvoie l'identifiant de la chanson la plus susceptible de matcher avec l'echantillon
   
-  //si on a un taux de matching supérieur à 80%
-  //on ajoute la chanson à une liste 
-  //puis on fait l'analyse temporelle dessus
-  return timeCoherency(sample,potentialMatching(sample,database))
-  }
-
-
-def potentialMatching(sample:Array[Array[Float]],database:Array[Array[Array[Float]]]):Array[Array[Array[Float]]]={
-  //et qui renvoie les morceaux susceptibles de collet au sample
-  //d'après la ressemblance des empreintes (sans coherence temporelle)
-  var empreintes=database
-  var potentialMatching:Array[Array[Array[Float]]]=Array(Array(Array()))
-  var j=0 //indice d'incrémentation de la liste potentialMatching
-  for (i<-0 to empreintes.length-1){
-    var song=empreintes(i).slice(1,empreintes.length)//on retire l'id de la chanson
-    if (matchingRate(sample,song)._1>=0.8){
-      potentialMatching(j)=empreintes(i)
-      j+=1
-      }
-    //si on a un taux de matching supérieur à 80%
-    //on ajoute la chanson à une liste 
-    }
-  return potentialMatching
-  }
-
-def timeCoherency(sample:Array[Array[Float]],potentialMatching:Array[Array[Array[Float]]]):Float={
-  //fonction qui prend en entree un sample et une liste de morceau matchant sur le plan frequenciel
-  //et qui renvoie, apres analyse temporelle
-  //l'ID de la chanson correspondant le plus
-  var nbMaxDelta:Array[Float]=Array()
-  for (i<-0 to potentialMatching.length-1){
-    //pour chaque morceau potentiellement ressemblant
-    var potentialSong=potentialMatching(i).slice(1,potentialMatching.length)
-    var correspondingTab=matchingRate(sample,potentialSong)._2
-    //tableau des correspondances de chaque empreinte de sample avec celles de song
-    var deltaTab:Array[Float]=Array()
-    for (j<-0 to correspondingTab.length-1){
-      deltaTab++computeDelta(correspondingTab(j))
-      //on ajoute a la liste des delta les delta pour chaque correspondance de sample
-      }
-    nbMaxDelta(i)=countDelta(deltaTab,maxDelta(deltaTab))
-    //on rajoute le nombre d'occurences du delta le plus présent dans deltaTab
-    //pour chaque chanson
-    }
-  return potentialMatching(indiceMax(nbMaxDelta))(0)(0)
-  }
-
-def indiceMax(T:Array[Float]):Int={
-  //fonction qui prend en entree une liste T
-  //et qui renvoie l'indice du maximum de la liste
-  var indice=0
-  for (i<-1 to T.length-1){
-    if (T(i)>T(indice)){
-      indice=i
-      }
-    }
-  return indice
-  }
-
-
-
-def matchingRate(sample : Array[Array[Float]], song : Array[Array[Float]]):(Float,Array[Array[Array[Float]]])={
-  //fonction qui compare un echantillon et une chanson entière
-  //et retourne leur taux de "matching" 
-  //ainsi que la liste des empreintes qui correspondent entre elles
-  //sous la forme [[[sampleEmpreinte1],[correspondingEmpreinte1A],[correspondingEmpreinte1B]],[[sampleEmpreinte1],[correspondingEmpreinte1A],[correspondingEmpreinte1B]]]
-  //la forme des données song et sample sont des tableaux de la forme
-  // [[f-ancrage,f-point,delta-temps],[f-ancrage,f-point,delta-temps]]
-  //ce sont donc deux tableaux contenant des empreintes
-  var rate:Float=0
-  var correspondingFingerprints:Array[Array[Array[Float]]]=Array(Array(Array()))
-  //tableau contenant la liste des empreintes de sample avec leurs correspondances dans song
-  var k=0 //indice pour remplir correspondingFingerprints avec chaque liste de correpondance
-  var l=1 //indice pour remplir correspondingFingerprints avec chaque correspondance
-  for (i<-0 to sample.length-1){
-    var rempli:Boolean=false//pour savoir si le sample d'indice i a une correspondance
-    correspondingFingerprints(k)(0)=sample(i) //on rajoute l'élément de sample
-    for (j<-0 to song.length-1){
-      if (sample(i)(0)==song(j)(0) && sample(i)(1)==song(j)(1)){
-        //on compare les deux fréquences présentes dans les empreintes
-        rate=rate+1 
-        //rq: on suppose qu'une empreinte ne peut être présente qu'une seule fois dans chaque liste  
-        correspondingFingerprints(k)(l)=song(j)
-        l+=1
-        rempli=true
+    def matchingRate(sample : Array[Double], song : Array[Double] ):(Double,ArrayBuffer[ArrayBuffer[Double]])= {
+    var t: ArrayBuffer[ArrayBuffer[Double]] = ArrayBuffer()
+    var somme : Int = 0 
+    var pris : Boolean = false
+    var cpt : Int = 0
+    for (i<- 0 to sample.length -1 by 3){
+       pris = false
+      for (j <- 0 to song.length -1 by 3){
+        if (environEgal(sample(i),song(j)) && environEgal(sample(i+1),song(j+1)) && !pris){
+           t append  ArrayBuffer(sample(i),sample(i+1),sample(i+2),song(j),song(j+1),song(j+2))
+          somme +=3
+          pris = true
+          cpt+=1
+        }
+        else if (environEgal(sample(i),song(j)) && environEgal(sample(i+1),song(j+1)) && pris){
+          t(cpt-1) append (song(j),song(j+1),song(j+2))
         }
       }
-    if (rempli){
-      k+=1
-      } 
-      //si on a eu des correspondances avec sample(i)
-      //on rempli correspondingFingerprints a la suite
-      //sinon on reprend cet indice k non rempli 
-      //et on l'utilise pour stocker les new valeurs de l'indice suivant
     }
-  var taux:Float=rate/sample.length
-  return (taux,correspondingFingerprints)
+    var taux = (somme/sample.length.toDouble)
+    return (taux,t)
   }
-  
-def computeDelta(T : Array[Array[Float]]):Array[Float]={
-  //fonction qui prend en entree une liste sous la forme
-  //[[sample],[correspondance1],[correspondance2]]
-  //et qui renvoie une liste de delta calculés chacuns pour une correspondance
-  var deltaTab:Array[Float]=Array()
-  for (i<-1 to T.length-1){
-      deltaTab(i-1)=math.abs(T(0)(2)-T(i)(2))
-      //on ajoute à deltaTab tous les delta de temps
-    }
-  return deltaTab
-  }  
-  
-def maxDelta(deltaTab:Array[Float]):Float={
+   def environEgal(a:Double,b:Double):Boolean={
+     return a<b+10.7 && a>b-10.7
+   }
+   //Renvoie le nombre de notes ayant le meme decalage temporel 
+   def nbOccurences(matchingRate:ArrayBuffer[ArrayBuffer[Double]]):Double={
+     var ref:Double = 0
+     var count : ArrayBuffer[Double] = ArrayBuffer()
+     for (i<-0 to matchingRate.length -1){
+       ref = matchingRate(i)(2)
+       for (j<-5 to matchingRate(i).length-1 by 3){
+         count append ref - matchingRate(i)(j)
+       }
+     }
+     var nbOccu = maxDelta(count)// complexité degeu ici pourrait etre mieux 
+     return countDelta(count,nbOccu)
+   }
+ def maxDelta(deltaTab:ArrayBuffer[Double]):Double={
 //fonction qui prend en argument une liste de delta 
 //et qui retourne la valeur ayant le plus d'occurences dans la liste
   var deltamax=deltaTab(0)
@@ -138,16 +55,67 @@ def maxDelta(deltaTab:Array[Float]):Float={
   return deltamax
   }
 
-def countDelta(deltaTab:Array[Float],delta:Float):Int={  
+def countDelta(deltaTab:ArrayBuffer[Double],delta:Double):Int={  
   //fonction qui prend en entrée une liste de deltas et une valeur de delta
   //et qui ressort le nombre d'occurences de cette valeur
   var countDelta:Int=0
   var deltamax=deltaTab(0)
-  for (i<-1 to deltaTab.length-1){
+  for (i<-0 to deltaTab.length-1){
     if (deltaTab(i)==delta){
       countDelta+=1
       }
     }
   return countDelta
   }
+//prends un sample et une database puis renvoie l'indice du tableau database correspondant le plus a sample
+  def compare(sample:Array[Double],database:Array[Array[Double]]):Int={
+    var id:ArrayBuffer[Int]=ArrayBuffer()
+    var nbOccu:ArrayBuffer[Double]=ArrayBuffer()
+    for (i<-0 to database.length -1){
+      var taux = matchingRate(sample,database(i))._1
+      var correspondingFingerprints = matchingRate(sample,database(i))._2
+      if (taux >=0.8){
+        id append i}
+    }
+    for (j<- 0 to id.length-1){
+      nbOccu append nbOccurences(matchingRate(sample,database(j))._2) 
+    }
+    var imax = indiceMax(nbOccu)
+    return id(imax)
+  }
+  
+def indiceMax(T:ArrayBuffer[Double]):Int={
+  //fonction qui prend en entree une liste T
+  //et qui renvoie l'indice du maximum de la liste
+  var indice=0
+  for (i<-1 to T.length-1){
+    if (T(i)>T(indice)){
+      indice=i
+      }
+    }
+  return indice
+  }
+
+/*var database =Array(Array(100.0 , 200.0 , 500.0 , 400.0 , 500.0 , 600.0 , 700.0 , 800.0 , 1000.0),Array(100.0 , 200.0 , 500.0 , 400.0 , 500.0 , 600.0 , 700.0 , 800.0 , 900.0))
+var sample = Array(100.0 , 200.0 , 500.0 , 400.0 , 500.0 , 600.0 , 700.0 , 800.0 , 900.0)
+println(compare(sample,database))*/
+ //var sample=Array.fill(12)(1000*math.random)
+ //var song=Array.fill(12)(1000*math.random)
+ /*var sample = Array(100.0 , 200.0 , 500.0 , 400.0 , 500.0 , 600.0 , 700.0 , 800.0 , 900.0)
+ var song = Array(100.0 , 200.0 , 300.0 , 400.0 , 500.0 , 300.0 , 700.0 , 900.0 , 900.0 ,100.0 ,200.0 ,1000.0, 400.0 , 500.0 , 1100.0 )
+  
+  println(sample.deep.mkString("\n"))
+  println("\n")
+  println(song.deep.mkString("\n"))
+  println("\n")
+ var taux = matchingRate(sample,song)._1
+ var correspondingFingerprints = matchingRate(sample,song)._2
+ println(taux)
+ println(correspondingFingerprints)
+ var cpt = nbOccurences(matchingRate(sample,song)._2)
+ println(cpt)*/
+/*var array = ArrayBuffer(3.0,5.0,3.0,3.0,6.0,7.0)
+println(array)
+println(maxDelta(array))
+println(countDelta(array,maxDelta(array)))*/
 }
